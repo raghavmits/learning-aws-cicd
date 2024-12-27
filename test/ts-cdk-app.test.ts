318fcf37e0b2b89d84497b95eb9260b6ba140588
Raghav Mittal <raghavmittal.wbs@gmail.com>
2024-12-27 12:21:54 -0800

    Initial commit

---
+++ test/ts-cdk-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TsCdkApp from '../lib/ts-cdk-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ts-cdk-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TsCdkApp.TsCdkAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
