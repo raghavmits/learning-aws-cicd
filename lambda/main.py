2203f5d288b7642b90cdc6647a01a8e088549a22
Raghav Mittal <raghavmittal.wbs@gmail.com>
2025-01-01 19:40:09 -0800

    Initial commit with lambda function, s3 bucket and open URL

---
+++ lambda/main.py
@@ -0,0 +1,3 @@
+def handler(event, context):
+    return {"statusCode": 200, "body": "I am a disco dancer" }
+
--- lib/ts-cdk-app-stack.ts
+++ lib/ts-cdk-app-stack.ts
@@ -1,16 +1,38 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class TsCdkAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, "MyFirstCDKBucket", {});
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TsCdkAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const lambdaFunction = new lambda.Function(this, "LambdaFunction", {
+      runtime: lambda.Runtime.PYTHON_3_10, 
+      code: lambda.Code.fromAsset("lambda"), 
+      handler: "main.handler"
+
+    });
+
+    const functionURL = lambdaFunction.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ["*"],
+        allowedMethods: [lambda.HttpMethod.ALL],
+        allowedHeaders: ["*"]
+      }
+    });
+
+    new cdk.CfnOutput(this, "Url", {
+      value: functionURL.url
+    });
+
+
+
+
+
   }
 }
