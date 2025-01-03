50a9acc83c5478fa67a99ad675673f004a5452be
Raghav Mittal <raghavmittal.wbs@gmail.com>
2025-01-02 16:19:01 -0800

    add visitor count

--- lambda/main.py
+++ lambda/main.py
@@ -1,10 +1,34 @@
 import os
+import boto3
+
 
 def handler(event, context):
+
+    # Raw event data
+    path = event["rawPath"]
+    if path != "/":
+        return {"statusCode": 404, "body": "Not Found."}
+
+
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table(os.environ.get("TABLE_NAME"))
+
+    # Read the "VISIT COUNT" key (or create it if it doen't exist)
+    response = table.get_item(Key={"key": "visit_count"})
+    if "Item" in response: 
+        visit_count = response["Item"]["value"]
+    else: 
+        visit_count = 0
+
+    # Increment the visit count and write it back to the table
+    new_visit_count = visit_count + 1
+    table.put_item(Item={"key": "visit_count", "value": new_visit_count})
+
     version =  os.environ.get("VERSION", "0.0")
     response_body = {
         "message": "Na kisi ka thikana!",
-        "version": version
+        "version": version, 
+        "visit_count": new_visit_count
 
     }
     return {"statusCode": 200, "body": response_body }
--- lib/ts-cdk-app-stack.ts
+++ lib/ts-cdk-app-stack.ts
@@ -3,6 +3,8 @@ import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as dotenv from "dotenv";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class TsCdkAppStack extends cdk.Stack {
@@ -11,6 +13,14 @@ export class TsCdkAppStack extends cdk.Stack {
 
     dotenv.config();
 
+    const table = new dynamodb.Table(this, "VisitorTimeTable", {
+      partitionKey: {
+        name: "key",
+        type: dynamodb.AttributeType.STRING
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
     const bucket = new s3.Bucket(this, "MyFirstCDKBucket", {});
 
     const lambdaFunction = new lambda.Function(this, "LambdaFunction", {
@@ -18,10 +28,13 @@ export class TsCdkAppStack extends cdk.Stack {
       code: lambda.Code.fromAsset("lambda"), 
       handler: "main.handler",
       environment: {
-        VERSION: process.env.VERSION || "0.0"
+        VERSION: process.env.VERSION || "0.0",
+        TABLE_NAME: table.tableName,
       }
 
     });
+
+    table.grantReadWriteData(lambdaFunction);
 
     const functionURL = lambdaFunction.addFunctionUrl({
       authType: lambda.FunctionUrlAuthType.NONE,
